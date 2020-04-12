e5fc5bea9b491238184e24c9e43dfd4db2449512
Alexander Bartolomey <occloxium@gmail.com>
2020-04-12 15:02:03 +0200

    Add support for rounded corners and parameter for fill color

--- main.py
+++ main.py
@@ -18,13 +18,14 @@ def create_buffer(data, delta, steps):
   print('Created sample buffer')
   return samples
 
-def draw(output_file, samples, step_width, step_height, color='black', gap=0):
+def draw(output_file, samples, step_width, step_height, color='white', gap=1, rounded=0):
   dwg = svgwrite.Drawing(output_file + '.svg')
   print('Drawing...')
   for i in range(0, len(samples)):
     dwg.add(dwg.rect(
-      (i * (step_width) + gap, (1 - samples[i]) * step_height), 
-      (step_width - 0.5*gap, samples[i] * step_height), 
+      (i * (step_width) + gap, (1 - samples[i]) * step_height), # position
+      (step_width - 0.5*gap, samples[i] * step_height), # size
+      rounded, rounded, # border radius (x, y)
       fill=color)
     )
 
@@ -48,6 +49,8 @@ def main():
   parser.add_argument('--width', help='The total width of the image')
   parser.add_argument('--height', help='The total height of the image')
   parser.add_argument('--output', help='Output file path')
+  parser.add_argument('--color', help="The fill color for the bars")
+  parser.add_argument('--rounded', help="Rounded corner radius")
   args = parser.parse_args()
 
   if args.input:
@@ -71,6 +74,16 @@ def main():
   else:
     step_width = 2000 / steps
 
+  if args.rounded:
+    rounded = abs(int(args.rounded))
+  else:
+    rounded = 0
+
+  if args.color:
+    color = args.color
+  else:
+    color = "black"
+
   x_gap = 0.2 * step_width
 
   if args.height:
@@ -85,7 +98,15 @@ def main():
 
   delta_t = len(y) // steps
   samples = create_buffer(y, delta_t, steps)
-  draw(output, samples, step_width, step_height, 'black', x_gap)
+  draw(
+    output_file=output, 
+    samples=samples,
+    step_width=step_width, 
+    step_height=step_height, 
+    color=color, 
+    gap=x_gap,
+    rounded=rounded
+  )
 
 if __name__ == '__main__':
     main()
