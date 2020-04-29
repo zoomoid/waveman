403c2cf59f13e6e32ddb9e7657a76beb70a36cf2
Alexander Bartolomey <occloxium@gmail.com>
2020-04-29 20:41:01 +0200

    Re-enable logging

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ def post_wavify():
         return svg, 200
 
 if __name__ == '__main__':
-  app.run(debug=False, host='0.0.0.0') # We need to listen on gateway for docker to tunnel correctly
+  app.run(debug=True, host='0.0.0.0') # We need to listen on gateway for docker to tunnel correctly
