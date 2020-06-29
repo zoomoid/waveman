98d41f39bf0852c5dd1985d9e8b952d7d9001fd3
Alexander Bartolomey <occloxium@gmail.com>
2020-06-29 17:07:23 +0200

    fix member function mismatch

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def wavify(wave: WaveBody):
   fn, response = transcode(wave.uri, return_response=True)
   if response.status_code == 200:
     # Generate SVG for sending back
-    small_waveform = gen_small_waveform(fn)
-    full_waveform = gen_full_waveform(fn)
+    small_waveform = gen_waveform(fn, "config/small.json")
+    full_waveform = gen_waveform(fn, "config/full.json")
     # cleanup afterwards
     cleanup(fn)
     return {"full": full_waveform, "small": small_waveform}
@@ -30,19 +30,11 @@ def wavify(wave: WaveBody):
     else:
       raise fastapi.HTTPException(status_code=response.status_code)
 
-def gen_full_waveform(filename):
-  with open("config/full.json", "r") as f:
-    full_config = json.load(f)
-  wave = waveman(filename, config=full_config)
-  svg = wave.to_string()
-  wave = None
-  return svg
-
-def gen_small_waveform(filename):
-  with open("config/small.json", "r") as f:
-    small_config = json.load(f)
-  wave = waveman(filename, config=small_config)
-  svg = waveman.to_string()
+def gen_waveform(filename, config_fn):
+  with open(config_fn, "r") as f:
+    cfg = json.load(f)
+  wave = waveman(filename, config=cfg)
+  svg = to_string(wave)
   wave = None
   return svg
 
