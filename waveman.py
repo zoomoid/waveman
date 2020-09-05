96be200ccea6168b1dacdb3ebd93502b79bab6d8
Alexander Bartolomey <occloxium@gmail.com>
2020-09-05 13:19:14 +0200

    :sparkles: improve waveman typing and class

--- waveman.py
+++ waveman.py
@@ -3,12 +3,31 @@ import soundfile
 import os
 import uuid
 import json
-from logger import log
-
-from requests import get
+from logger import Logger
 from pydub import AudioSegment
+from config import Config, Small, Full
 
-CONFIG = {}
+class Position:
+  def __init__(self, x, y)
+    self._x = x
+    self._y = y
+  @property
+  def x(self):
+    return self._x
+  @property 
+  def y(self):
+    return self._y
+
+class Size:
+  def __init__(self, w, h):
+    self._w = w
+    self._h = h
+  @property
+  def w(self):
+    return self._w
+  @property
+  def h(self):
+    return self._h
 
 def waveman(fn, config=None):
   if config == None:
@@ -17,38 +36,42 @@ def waveman(fn, config=None):
   else:
     CONFIG = config
   CONFIG['width'] = CONFIG['steps'] * CONFIG['step_width']
-  log("Initializing new drawing context", file=fn)  
+  # log("Initializing new drawing context", file=fn)  
   canvas = svgwrite.Drawing(profile='tiny', viewBox=f"0 0 {CONFIG['width']} {CONFIG['height']}", preserveAspectRatio=CONFIG["preserveAspectRatio"])
   sf = soundfile.SoundFile(fn)
-  total_samples = len(sf)
+  total_samples = sf.frames
   block_length = int(total_samples // CONFIG['steps'])
   f = open(fn, "rb")
   block_iterator = soundfile.blocks(f, blocksize=block_length)
   chunks = []
   for i, block in enumerate(block_iterator):
-    mono_block = list(map(lambda sample: (sample[0] + sample[1]) / 2, block))
-    chunks.append(transformer(mono_block, CONFIG['mode']))
-    log("Reduced frame to sample", i=i)
+    mono_block = list(map(lambda sample: abs((sample[0] + sample[1]) / 2), block))
+    chunks.append(transformer(mono_block, CONFIG['mode'], block_length))
   chunks = normalize(chunks)
-  log("Tranformed frames into chunks")
+  # log("Tranformed frames into chunks")
   for i, chunk in enumerate(chunks):
     canvas.add(artist(canvas, chunk, i, CONFIG['step_width'], CONFIG['height'], CONFIG['gap'], CONFIG['align'], CONFIG['rounded'], "#abcdef"))
-  log("Created SVG rectangles for all data chunks")
+  # log("Created SVG rectangles for all data chunks")
   return canvas
 
-def normalize(chunk):
-  max_val = max([abs(s) for s in chunk])
-  if max_val == 0:
-    raise ArithmeticError
-  return list(map(lambda v: v / max_val, chunk))
+"""
+Normalizes a given list of numbers
+"""
+def normalize(l):
+  m = max([abs(x) for x in l])
+  return [x / m for x in l]
 
-def transformer(chunk, mode):
+"""
+Reduces a given chunk of samples according to a given mode, i.e.,
+avg, max, and rounded_avg
+"""
+def transformer(chunk, mode, length):
   def avg(_chunk):
-    return sum([abs(s) for s in _chunk]) / len(_chunk) 
+    return sum(_chunk) / length 
   def rounded_avg(_chunk):
-    return round(sum([abs(s) for s in _chunk]) / len(_chunk), 2)
+    return round(sum(_chunk) / length, 3)
   def max(_chunk):
-    return max([abs(s) for s in _chunk])
+    return max(_chunk)
 
   if mode == "max":
     chunk = max(chunk)
@@ -60,35 +83,19 @@ def transformer(chunk, mode):
     raise TypeError
   return chunk
 
-def artist(canvas, chunk, i, width, height, gap, align, rounded, color):
-  def bottom(chunk, i):
-    pos = (i * (width),  (1 - chunk) * height)
-    size = (width - gap, chunk * height)
-    return pos, size
-  def center(chunk, i):
-    pos =  (i * (width), (0.5 * height) - (0.5 * chunk * height))
-    size = (width - gap, chunk * height)
-    return pos, size
-  if width == None or height == None or gap == None or align == None or rounded == None or color == None:
-    raise TypeError
-  log("Drawing chunk", i=i)
-  if align == "bottom":
-    pos, size = bottom(chunk, i)
-  elif align == "center":
-    pos, size = center(chunk, i)
-  else:
-    raise TypeError
-  return canvas.rect(pos, size, rounded, rounded, fill=color)
-
+"""
+Transcodes an mp3 file located at vol/fn to a waveform which we can process using streams
+""" 
 def transcode(url):
-  output_fn = str(uuid.uuid4())[0:8]
+  output_fn = str(uuid.uuid4())
   sound = AudioSegment.from_mp3(f"files/{url}")
-  sound.export(f"files/{output_fn}.wav", format="wav")
-  return f"files/{output_fn}.wav"
+  sound.export(f"/tmp/{output_fn}.wav", format="wav")
+  Logger.info("Finished transcoding mp3 to wav", source=fn, target=f"{output_fn}.wav")
+  return f"/tmp/{output_fn}.wav"
 
-def to_string(canvas):
-  return canvas.tostring().replace("#abcdef", "{{.color}}")
-
+"""
+Simply removes a file located at fn
+"""
 def cleanup(fn):
   os.unlink(fn)
   return True
